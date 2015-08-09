6ea21102a92af4d54b9a45dec28f8131c95a58b2
Philron William Hozier <phozier@Philrons-Air.home>
2015-08-09 11:13:39 -0400

    initial ~/api commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify, request, abort
+import psqldb 
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/ios/api/v1/create_db')
+def index():
+	psqldb.create_schema()
+	psqldb.print_response(cursor)
+	return "{'init': {'conn_string':ok, 'schema':ok}"
+	
+# curl -i -H "Content-Type: application/json" -X POST -d  '{"uid":"btaylor@wpi.edu", "lname":"taylor", "fname":"bruce", "password":darkknight"}' http://localhost:3001/ios/api/v1/user
+# ?uid=<uid_param>&lname=<lname_param>&fname=<fname_param>&password=<password_param>
+@app.route('/ios/api/v1/user', methods=['POST'])
+def create_user():
+
+	if len(request.args) == 4:
+		print 'we win!!'
+		'''
+		usage: 
+			requests.post("http://localhost:3001/ios/api/v1/user", params=payload)
+			payload = {"lname":"thanks", "fname":"cold", "password":"cmit"}
+		'''
+		return psqldb.insert(request.args.get('uid'), request.args.get('lname'), request.args.get('fname'), request.args.get('param'))
+ 	 
+	resp = jsonify({'message':'All fields must have values.'})
+	resp.status_code = 404
+	return resp
+	
+
+@app.route('/ios/api/v1/select/<user_id>', methods=['GET']) 
+def response(user_id):
+	return jsonify(psqldb.select(user_id))
+
+@app.route('/ios/api/v1/list_all', methods=['GET']) 
+def list():
+	return psqldb.list_all()
+	
+if __name__ == '__main__':
+	app.run(port=3001, debug=True)
+	
+	
---
+++ psqldb.py
@@ -0,0 +1,99 @@
+import os
+import psycopg2
+import urlparse
+from flask import jsonify
+import random
+ 
+def connect():
+	os.environ["DATABASE_URL"] = "postgres://ehaxtcxuugahlv:6vv92WKuRCvNV6-1gMHlbbeOMM@ec2-54-83-10-210.compute-1.amazonaws.com:5432/d5cd7ej8t9fbns"
+	urlparse.uses_netloc.append("postgres")
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
+	connect = psycopg2.connect(
+	    database=url.path[1:],
+	    user=url.username,
+	    password=url.password,
+	    host=url.hostname,
+	    port=url.port
+	)
+	
+	print connect
+	return connect
+
+
+def dictionary(values, keys):
+	data = {}
+	index = 0
+	if not values:
+		data = {}
+	elif len(values[0]) == len(keys):
+		for row in values:
+			a_user_record = {}
+			for i in range(len(keys)):
+				import re
+				extracted_key = " ".join(re.findall("[a-zA-Z]+", str(keys[i])))
+				a_user_record[extracted_key] = row[i]
+			data["ID{0}".format(index)] = (a_user_record)
+			index+=1
+			
+	return data
+	
+	
+''' overview: returns cursor object'''
+def create_schema():
+	conn = connect()
+	cursor = conn.cursor()
+	cursor.execute('''
+		Create table user_db(
+		uid text primary key		not null,
+		lname text 				not null,
+		fname text 				not null,
+		password text			not null
+		);
+		
+	''')
+	
+	conn.commit()
+	conn.close()
+	
+def insert(uid, lname, fname, password):
+	conn = connect()
+	cursor = conn.cursor()
+	query_string = "INSERT INTO user_db (uid,lname,fname,password) \
+      VALUES ('{0}', '{1}', '{2}', '{3}' )".format(uid, lname, fname, password)
+	 
+	verify = select(uid)
+	
+	if not verify:
+		cursor.execute(query_string);
+		conn.commit()
+		conn.close()
+		return jsonify(verify)
+	else:
+		conn.commit()
+		conn.close()
+		return jsonify({"Message":"uid already exists"}) #revisit
+
+def select(uid):
+	conn = connect()
+	cursor = conn.cursor()
+	query_string = "select * from user_db WHERE uid = '{0}'". format(uid) 
+	cursor.execute(query_string)
+	values = cursor.fetchall()
+
+	query_string = "SELECT column_name FROM information_schema.columns WHERE table_name ='user_db'"
+	cursor.execute(query_string)
+	keys = cursor.fetchall()
+			
+	return dictionary(values, keys)
+	
+def list_all():
+	conn = connect()
+	cursor = conn.cursor()
+	query_string = "select * from user_db "
+	cursor.execute(query_string)
+	values = cursor.fetchall()
+	query_string = "SELECT column_name FROM information_schema.columns WHERE table_name ='user_db'"
+	cursor.execute(query_string)
+	keys = cursor.fetchall()
+			
+	return jsonify(dictionary(values, keys))
