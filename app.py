eca224981e5aad6598f5bf27e0e579a0bc430e24
Philron William Hozier <phozier@1x-vl923-72-19-108-218.wireless.umass.edu>
2015-10-25 20:16:46 -0400

    extension of API includes new tables, card button logic, automated timestamps serverside to node_db and more

--- app.py
+++ app.py
@@ -1,53 +1,83 @@
 from flask import Flask, jsonify, request, abort
-import psqldb 
+import psqldb
 
 
 app = Flask(__name__)
 
 @app.route('/ios/api/v1/create_db')
 def index():
-	return psqldb.create_schema('user_db')
-	
-	
+	# return psqldb.create_schema('user_db')
+	return psqldb.create_schema('note_metrics')
+
 @app.route('/ios/api/v1/user', methods=['POST'])
 def api_user_control():
-	
+
 	if len(request.args) == 3:
 		print 'we win!!'
 		'''
-		usage: 
+		usage:
 			requests.post("http://localhost:3001/ios/api/v1/user", params=payload)
 			payload = {"uid":"thanks@me.com", "nickname":"cold", "password":"cmit"}
 		'''
 		return psqldb.insert(request.args.get('uid'), request.args.get('nickname'), request.args.get('password'))
- 	 
-	return jsonify({'message':'All fields must have values.'}), 404
-	
-	
 
-@app.route('/ios/api/v1/select/<user_id>', methods=['GET', 'DELETE']) 
+	return jsonify({'message':'All fields must have values.'}), 404
+
+
+@app.route('/ios/api/v1/note', methods=['POST'])
+def new_note():
+	if len(request.args) == 2:
+		print 'we win!!'
+		'''
+		usage:
+			requests.post("http://localhost:3001/ios/api/v1/note", params=payload)
+			payload = {"uid":"thanks@me.com", "note":"no class today!!"}
+		'''
+		return psqldb.make_note(request.args.get('uid'), request.args.get('note'))
+	return jsonify({'message':'All fields must have values.'}), 404
+
+
+@app.route('/ios/api/v1/note_metric', methods=['POST'])
+def modify_note_metric():
+	if len(request.args) == 2:
+		print 'we win!!'
+		'''
+		usage:
+			requests.post("http://localhost:3001/ios/api/v1/note_metric", params=payload)
+			payload = {"note_id":"3", "tag":"down_btn_tag"}
+		'''
+		return psqldb.update_note_metric(request.args.get('note_id'), request.args.get('tag'))
+	return jsonify({'message':'All fields must have values.'}), 404
+
+
+@app.route('/ios/api/v1/select/<user_id>', methods=['GET', 'DELETE'])
 def response(user_id):
 	if request.method == 'GET':
-		return psqldb.select(user_id)
-	
+		json = psqldb.select(user_id)
+		if not json:
+			return jsonify({"message":"no user found"}), 404
+		return jsonify({"user":json})
+
 	elif request.method == 'DELETE':
 		return psqldb.delete(user_id)
-		
-	
-	
 
-@app.route('/ios/api/v1/list_all', methods=['GET']) 
+
+
+
+@app.route('/ios/api/v1/list_all', methods=['GET'])
 def list():
 	return psqldb.list_all()
-	
+
+@app.route('/ios/api/v1/list_all_notes', methods=['GET'])
+def listn():
+	return psqldb.list_all_notes()
+
 @app.route('/ios/api/v1/login', methods=['POST'])
 def api_login_control():
 	if len(request.args) == 2:
 		return psqldb.login(request.args.get('uid'), request.args.get('password'))
-		
+
 	return jsonify({'message':'All fields must have values.'}), 404
-		
+
 if __name__ == '__main__':
 	app.run(port=3001, debug=True)
-	
-	
--- psqldb.py
+++ psqldb.py
@@ -4,7 +4,8 @@ import psycopg2.extras
 import urlparse
 from flask import jsonify
 from psycopg2.extensions import adapt
- 
+import datetime
+
 def connect():
 	os.environ["DATABASE_URL"] = "postgres://ehaxtcxuugahlv:6vv92WKuRCvNV6-1gMHlbbeOMM@ec2-54-83-10-210.compute-1.amazonaws.com:5432/d5cd7ej8t9fbns"
 	urlparse.uses_netloc.append("postgres")
@@ -16,75 +17,97 @@ def connect():
 	    host=url.hostname,
 	    port=url.port
 	)
-	
+
 	return connect
-		
+
 ''' overview: returns cursor object'''
+''' TODO: schema that also guards against other users voting more than once.'''
 def create_schema(new_table_name):
 	conn = connect()
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 	cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'") # lists just the table(s) that the admin creates.
-	
+
 	if new_table_name == cursor.fetchone()['table_name']:
 		end(conn)
 		return jsonify({"message":"table already exists"}), 404
-	
+
 	cursor.execute('''
 		Create table {0}(
-		uid text primary key		not null,
-		nickname text 				not null,
-		password text			not null
+        note_id 			serial primary key,
+		uid text              		not null,
+		nickname text              		not null,
+        note varchar(99)            not null,
+		down smallint 				DEFAULT '0' ,
+		up smallint					DEFAULT '0',
+		timestamped text
 		);
-		
+
 	''' . format(new_table_name))
-	
+
 	end(conn)
 	return jsonify({"message":"new table created."})
 
-def is_present(uid):
+def is_present(column, table, selection_item):
 	conn = connect()
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 	collection = {"conn":conn, "boolean":True, "cursor":cursor}
-	query_string = "select uid from user_db WHERE uid = '{0}'". format(uid) 
+	query_string = "select {0} from {1} WHERE {0} = '{2}'". format(column, table, selection_item)
 	cursor.execute(query_string)
 	json = cursor.fetchone()
-	
+
 	if not json: # if response from query is '[]' -- the empty array, then
 		collection["boolean"] = False
 	return collection
 
 
-def insert(uid, nickname, password):
-	verify = is_present(uid)
+def make_note(uid, note):
+	'''overview:
+	    api: is_present(column /*string*/, table /*string*/, selection_item):
+	'''
+	verify = is_present('note', 'note_metrics', note)
+	timestamped = str(datetime.datetime.now())
+
+	'''if the note does not exists, insert note.'''
 	if verify['boolean'] == False:
-		query_string = """INSERT INTO user_db (uid, nickname, password) \
-      	VALUES ({0}, {1}, {2})""".format(adapt(uid), adapt(nickname), adapt(password))
+		query_string = """INSERT INTO note_metrics (uid, nickname, note, \
+		timestamped) VALUES ({0}, {1}, {2}, {3})""".format(adapt(uid),
+		adapt(select(uid)["nickname"]), adapt(note), adapt(timestamped))
 		verify["cursor"].execute(query_string);
 		end(verify["conn"])
-		return jsonify({"message":"new user created"})
-		
+		return jsonify({"message":"new note created"})
 	else:
 		end(verify["conn"])
 		return jsonify({"message":"uid already exists"}), 404
 
-def login(uid, password):
+def update_note_metric(note_id, tag):
 	conn = connect()
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-	query_string = "select * from user_db WHERE uid = '{0}'". format(uid) 
-	
+	query_string = "select note_id from note_metrics WHERE note_id = '{0}'". format(note_id)
 	cursor.execute(query_string)
-	selected_user = cursor.fetchone()
-	
-	if selected_user and selected_user["password"] == password:
-		selected_user.pop("password", None) # keep server side password client side
-		return jsonify({"message":"user and password verified", 
-						"user":selected_user}) 
-	return jsonify({"message":"login unsuccessful"}), 404
-	
-def select(uid):
+	note = cursor.fetchone()
+
+	new_up = 0
+	new_down = 0
+	if tag == 200:
+		if note["down"] == 1:
+			new_down = note["down"] - 1
+	elif tag == 201:
+		if(note["up"] == 0):
+			new_up = note["up"] + 1
+
+	query_string = """UPDATE note_metrics set down = {0}, \
+					set up = {1} where note_id = {2}""" \
+					.format(new_down, new_up, note_id)
+	cursor.execute(query_string)
+	end(conn)
+	return jsonify({"message":"up: {0}, down: {1},\
+					 note_id: {2}".format(new_up, new_down, note_id)})
+
+
+def select_note(note_id):
 	conn = connect()
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-	query_string = "select uid, nickname from user_db WHERE uid = '{0}'". format(uid) 
+	query_string = "select note_id from note_metrics WHERE note_id = '{0}'". format(note_id)
 	cursor.execute(query_string)
 	json = cursor.fetchone()
 	end(conn)
@@ -92,24 +115,71 @@ def select(uid):
 	if not json:
 		return jsonify({"message":"no user found"}), 404
 	return jsonify({"user":json})
-	
-	
+
+def insert(uid, nickname, password):
+	verify = is_present("uid", 'user_db', uid)
+	if verify['boolean'] == False:
+		query_string = """INSERT INTO user_db (uid, nickname, password) \
+      	VALUES ({0}, {1}, {2})""".format(adapt(uid), adapt(nickname), adapt(password))
+		verify["cursor"].execute(query_string);
+		end(verify["conn"])
+		return jsonify({"message":"new user created"})
+
+	else:
+		end(verify["conn"])
+		return jsonify({"message":"uid already exists"}), 404
+
+def login(uid, password):
+	conn = connect()
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+	query_string = "select * from user_db WHERE uid = '{0}'". format(uid)
+
+	cursor.execute(query_string)
+	selected_user = cursor.fetchone()
+
+	if selected_user and selected_user["password"] == password:
+		selected_user.pop("password", None) # keep server side password client side
+		return jsonify({"message":"user and password verified",
+						"user":selected_user})
+	return jsonify({"message":"login unsuccessful"}), 404
+
+
+def select(uid):
+	conn = connect()
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+	query_string = "select uid, nickname from user_db WHERE uid = '{0}'". format(uid)
+	cursor.execute(query_string)
+	json = cursor.fetchone()
+	end(conn)
+	return json
+
 def list_all():
 	conn = connect()
 	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 	query_string = "select uid, nickname from user_db "
 	cursor.execute(query_string)
 	json = cursor.fetchall()
-	end(conn)	
-		
+	end(conn)
+
 	if not json:
 		return jsonify({"message":"user_db is empty"}), 404
 	return jsonify({"users":json})
-	
-	
-def delete(uid):	
-	
-	verify = is_present(uid) 
+
+def list_all_notes():
+	conn = connect()
+	cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+	query_string = "select * from note_metrics "
+	cursor.execute(query_string)
+	json = cursor.fetchall()
+	end(conn)
+
+	if not json:
+		return jsonify({"message":"user_db is empty"}), 404
+	return jsonify({"users":json})
+
+def delete(uid):
+
+	verify = is_present("uid", 'user_db', uid)
 	if verify['boolean'] == False: # if user is not present
 		return jsonify({"message":"no rows affected -- user not found"}), 404
 	else:
@@ -121,7 +191,7 @@ def delete(uid):
 def end(conn):
 		conn.commit()
 		conn.close()
-	
+
 ''' for admin purposes only. this function should never be placed permanently under any route. '''
 def _alter_psql_interface():
 	'''
@@ -135,4 +205,3 @@ def _alter_psql_interface():
 	end(conn)
 	'''
 	return jsonify({"message":"schema altered"})
-	
